c64f0b300a76b5986e56624886ea4b4aa2f81f8f
Monir <monir198@gmail.com>
2025-02-03 21:40:06 +0600

    initial commit

---
+++ src/main/java/com/techweave/spring_transaction_practise/SpringTransactionPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.techweave.spring_transaction_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techweave/spring_transaction_practise/SpringTransactionPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techweave.spring_transaction_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
