95b4211e4bcf5bd029f38f5b1dac96eb5284b89b
Monir <monir.bhuiyanbjitgroup.com>
2025-02-04 17:08:50 +0600

    Added domain and controller

---
+++ src/main/java/com/techweave/spring_transaction_practise/controller/OrderManagingController.java
@@ -0,0 +1,9 @@
+package com.techweave.spring_transaction_practise.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderManagingController {
+}
